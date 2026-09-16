eb10b87229fea2b80c0a91995c9658cbe16c7180
Rahul Guha <59439597+rgwonder786@users.noreply.github.com>
2026-09-16 21:27:09 +0530

    Print Numbers

---
+++ Day 1/a3.java
@@ -0,0 +1,16 @@
+//Print Numbers 
+public class a3 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println(3);
+        System.out.println(3.48);
+        System.out.println(33333.489);
+        System.out.println(3+3);
+        System.out.println(3*3);
+    }
+}
+
+
+//Java Comments
+/*Java Comments2 */
