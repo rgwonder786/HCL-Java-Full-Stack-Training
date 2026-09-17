5a08a9650411cd37b62b6eec1c63b3f745bbb29d
Rahul Guha <59439597+rgwonder786@users.noreply.github.com>
2026-09-17 19:57:23 +0530

    Using "return" in a method

---
+++ Day 2/a25.java
@@ -0,0 +1,17 @@
+/*
+Using return in a method
+*/
+public class a25 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Result: " + addNumbers(4, 5));
+    }
+
+    public static int addNumbers(int a, int b) {
+        // int sum = a + b;
+        // return sum;
+        return a + b;
+    }
+
+}
