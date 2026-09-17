3696247f681038a4221737b04a80783d735475fb
Rahul Guha <59439597+rgwonder786@users.noreply.github.com>
2026-09-17 05:33:11 +0530

    Pring Strings

---
+++ Day 1/a5.java
@@ -0,0 +1,17 @@
+// Pring Strings
+public class a5 {
+    
+    public static void main(String[] args) {
+         
+        String fname = "Rahul";
+        String lname = "Guha";
+        String fullame = fname +" "+lname;
+        System.out.println("The Full-Name is : " + fullame);
+
+        int x = 2;
+        int y = 3;
+        int z = 5;
+        System.out.println("The sum is : " + x + y + z);
+
+    }
+}
