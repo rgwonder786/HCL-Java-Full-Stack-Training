7af0c744775645e3ce1d1d48101990da9942db22
Rahul Guha <59439597+rgwonder786@users.noreply.github.com>
2026-09-17 19:49:21 +0530

    "if" Statement in Java

---
+++ Day 2/a13.java
@@ -0,0 +1,14 @@
+//"if" Statement in Java 
+// The if statement in Java evaluates a boolean condition.
+// If the condition is true, the block of code inside the if statement is executed.
+// If there is only one statement in if block, then curly braces {} are optional.
+public class a13 {
+
+    public static void main(String[] args) {
+
+        int number = 10;
+        if (number > 0) {
+            System.out.println("The no is Positive");
+        }
+    }
+}
