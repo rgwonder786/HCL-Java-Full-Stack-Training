39f8ac9e343877b38a10b48fd1a96bbe41f46cf2
Rahul Guha <59439597+rgwonder786@users.noreply.github.com>
2026-09-17 05:34:28 +0530

    Java Constant

---
+++ Day 1/a7.java
@@ -0,0 +1,21 @@
+// Java Constant
+public class a7 {
+    
+    public static void main(String[] args) {
+        
+        final int a = 4;
+        //a =20;
+        final char b = 'A';
+       
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+/*
+Constants (final keyword)
+When you do not want a variable's value to change, use the final keyword.
+
+A variable declared with final becomes a constant, which means unchangeable and read-only: You should declare variables as final when their values should never change.
+*/
+
+// Task: Calculate the area of a rectangle
