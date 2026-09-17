fa0057296983f8510fe14921ca74e3c6ad56f3f3
Rahul Guha <59439597+rgwonder786@users.noreply.github.com>
2026-09-17 05:36:29 +0530

    Primitive Data Type

---
+++ Day 1/a8.java
@@ -0,0 +1,31 @@
+//Java Data Types
+//Use of 
+
+/*
+Data Type	Description
+byte	    Stores whole numbers from -128 to 127
+short	    Stores whole numbers from -32,768 to 32,767
+int	        Stores whole numbers from -2,147,483,648 to 2,147,483,647
+long	    Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+float	    Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+double	    Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits
+boolean	    Stores true or false values
+char	     Stores a single character/letter or ASCII values
+*/
+
+//Primitive Data Type
+
+public class a8 {
+    public static void main(String[] args) {
+
+        byte age = 25;
+        short salary = 30000;
+        int population = 1500000;
+        long distance = 9876543210L;
+
+        System.out.println("Byte: " + age);
+        System.out.println("Short: " + salary);
+        System.out.println("Int: " + population);
+        System.out.println("Long: " + distance);
+    }
+}
