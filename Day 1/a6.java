232bdbf63654e13251019682b3191365e4d5e9fe
Rahul Guha <59439597+rgwonder786@users.noreply.github.com>
2026-09-17 05:33:49 +0530

    Identifiers

---
+++ Day 1/a6.java
@@ -0,0 +1,18 @@
+//Identifier
+public class a6 {
+
+    public static void main(String[] args) {
+
+        boolean isJavaGood = true;
+        boolean isJavabad = false;
+
+        System.out.println("I know you say: " + isJavaGood);
+        System.out.println("I know you say: " + isJavabad);
+    }
+}
+
+// a6 → Class identifier
+// main → Method identifier
+// args → Parameter identifier
+// isJavaGood → Variable identifier
+// isJavabad → Variable identifier
