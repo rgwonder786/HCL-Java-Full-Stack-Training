397ee53d9299f56289608da2c1134e06f5f5b7f4
Rahul Guha <59439597+rgwonder786@users.noreply.github.com>
2026-09-17 05:32:40 +0530

    Java variables

---
+++ Day 1/a4.java
@@ -0,0 +1,15 @@
+//Java variables
+public class a4 {
+    
+    public static void main(String[] args) 
+    {
+        
+        int age = 24;
+        float num = 30.48f;
+        char word = 'A';
+        String name = "Rahul Guha";
+        double d = 3333.44d;
+        boolean b = true;
+        System.out.println("My Name is : " + name +" "+ "I say: " + word + "My Age : "+ age +" "+ num + d + b);
+    }
+}
