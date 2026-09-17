4b1548cf62c260a7cae95ecb589fa07f64db9726
Rahul Guha <59439597+rgwonder786@users.noreply.github.com>
2026-09-17 05:38:50 +0530

    Narrowing Casting

---
+++ Day 1/a12.java
@@ -0,0 +1,13 @@
+//Narrowing Casting (manual)
+public class a12 {
+    
+    public static void main(String[] args) {
+        
+       double d = 99.99d;
+       int myNum = (int) d;
+
+       System.out.println(d);
+       System.out.println(myNum);
+
+    }
+}
