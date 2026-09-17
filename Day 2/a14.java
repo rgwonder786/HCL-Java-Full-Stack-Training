83db3befb572e1b7be084de542365e1d8b3d0419
Rahul Guha <59439597+rgwonder786@users.noreply.github.com>
2026-09-17 19:49:51 +0530

    "if-else" Statement in Java

---
+++ Day 2/a14.java
@@ -0,0 +1,15 @@
+//"if-else" Statement in Java
+// The if-else statement in Java evaluates a boolean condition.
+// If the condition is true, the block of code inside the if is executed; otherwise, the code inside the else block runs.
+
+public class a14 {
+
+    public static void main(String[] args) {
+        int number = -5;
+        if (number > 0) {
+            System.out.println("The No is Positive");
+        } else {
+            System.out.println("The no is Negative");
+        }
+    }
+}
